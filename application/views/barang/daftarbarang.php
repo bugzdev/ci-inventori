b59304366c12b01be2aed785e321d13afa90bdd7
h4km <hakamibrahim7@gmail.com>
2016-09-05 21:56:02 +0700

    delete data g penting

--- application/views/barang/daftarbarang.php
+++
@@ -1,9 +0,0 @@
-<h3><?php echo $title; ?></h3>
-Jumlah Data : <?php echo $list_barang->num_rows(); ?>
-<ul>
-  <?php
-    foreach ($list_barang->result() as $r) {
-      echo "<li>".$r->nama_barang."</li>";
-    }
-  ?>
-</ul>
