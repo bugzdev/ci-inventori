7939d375a641c838279bfbd7c9f204522b62436b
h4km <hakamibrahim7@gmail.com>
2016-09-08 07:18:00 +0700

    new file

---
+++ application/views/kategori/form_edit.php
@@ -0,0 +1,20 @@
+<h3>Edit Data Kategori</h3>
+
+<?php
+echo form_open('kategori/edit');
+?>
+
+<table border="1">
+  <tr>
+    <td>Nama Kategori</td>
+    <td><input type="text" name="kategori" placeholder="Kategori" value="<?php echo $record['nama_kategori']?>"></td>
+  </tr>
+
+  <tr>
+    <td colspan="2">
+      <button type="submit" name="submit">Edit</button>
+    </td>
+  </tr>
+</table>
+
+</form>
---
+++ application/views/kategori/list_data.php
@@ -0,0 +1,22 @@
+<h3>Kategori Barang</h3>
+<?php
+echo anchor('kategori/post','Tambah Data');
+?>
+<table border="1">
+  <tr><th>No</th><th>Nama Lengkap</th><th>Username</th><th>Login Terakhir</th><th colspan="2">Operasi</th></tr>
+   <?php
+   $no=1;
+   foreach ($record->result() as $r)
+   {
+       echo "<tr>
+           <td width='10'>$no</td>
+           <td>$r->nama_lengkap</td>
+           <td>$r->username</td>
+           <td>$r->last_login</td>
+           <td  width='20'>".anchor('operator/edit/'.$r->operator_id,'Edit')."</td>
+           <td  width='20'>".anchor('operator/delete/'.$r->operator_id,'Delete')."</td>
+           </tr>";
+       $no++;
+   }
+   ?>
+</table>
